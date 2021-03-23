4392e592dedcc6349932e0218b8b811c1b19c3cb
Pikita-Mikita <80912640+Pikita-Mikita@users.noreply.github.com>
2021-03-23 19:47:17 +0300

    Все файлы

--- threeNumber.py
+++ threeNumber.py
@@ -2,4 +2,9 @@ numb = input("Введите трехзначное число:")
 while len(numb) > 3:
     numb = input("Введите трехзначное число:")
 else:
-    
+    if numb[0] == numb[1] == numb[2]:
+        print("Все цифры в числе равны")
+    else:
+        print("Цифры в числе не равны")
+
+
